4a16b9c1f5ade6d9653e42117202c602f5e2dbdd
jghibiki <jghibiki.games@gmail.com>
2025-09-04 13:06:45 -0500

    add docs directory with the built site

--- .eleventy.js
+++ .eleventy.js
@@ -4,27 +4,33 @@ import postcss from 'postcss';
 import tailwindcss from '@tailwindcss/postcss';
 
 
-export default async function(eleventyConfig) {
+export default async function (eleventyConfig) {
 	eleventyConfig.setInputDirectory("src")
 
 	eleventyConfig.on('eleventy.before', async () => {
 		const tailwindInputPath = path.resolve('./src/styles/index.css');
-		const tailwindOutputPath = './_site/styles/index.css';
+		const tailwindOutputPath = './docs/styles/index.css';
 		const cssContent = fs.readFileSync(tailwindInputPath, 'utf8');
 		const outputDir = path.dirname(tailwindOutputPath);
-	
+
 		if (!fs.existsSync(outputDir)) {
 			fs.mkdirSync(outputDir, { recursive: true });
 		}
-	
+
 		const result = await postcss([tailwindcss()]).process(cssContent, {
 			from: tailwindInputPath,
 			to: tailwindOutputPath,
 		});
-	
+
 		fs.writeFileSync(tailwindOutputPath, result.css);
 	});
 
+	return {
+		dir: {
+			output: "docs",
+		}
+		//pathPrefix: "/MN-ARRL-Site/"
+	}
 };
 
 
